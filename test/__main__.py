846e93b7266e734429b8009aeb5af39ba06e4daf
rkluzinski <ryankluzinski@gmail.com>
2018-10-26 08:02:02 -0600

    changed automatic testing using python instead of make

---
+++ test/__main__.py
@@ -0,0 +1,16 @@
+import subprocess
+
+command = "./brainfast tests/{0}.b"
+
+programs = [
+    "bitwidth"
+    ]
+
+def main():
+    for program in programs:
+        print("Running {}.b: ".format(program), end="", flush=True)
+        subprocess.call(command.format(program), shell=True)
+
+
+if __name__ == "__main__":
+    main()
